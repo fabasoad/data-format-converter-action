29c6dc885b1d4a46c14e617ff42d1a354855eac0
fabasoad <fabasoad@gmail.com>
2025-01-14 23:27:51 +0900

    Cleanup

--- src/collect-info.sh
+++ src/collect-info.sh
@@ -7,23 +7,29 @@ LIB_DIR_PATH="${SRC_DIR_PATH}/lib"
 . "${LIB_DIR_PATH}/logging.sh"
 
 main() {
-  if command -v yq >/dev/null 2>&1; then
-    yq_installed="true"
-    log_info "yq is found at $(which yq). Installation skipped."
-  else
-    yq_installed="false"
-    log_info "yq is not found. Executing installation."
+  input_from="${1}"
+  input_to="${2}"
+
+  if [ "${input_from}" != "${input_to}" ]; then
+    if command -v yq >/dev/null 2>&1; then
+      yq_installed="true"
+      log_info "yq is found at $(which yq). Installation skipped."
+    else
+      yq_installed="false"
+      log_info "yq is not found. Executing installation."
+    fi
+    echo "yq-installed=${yq_installed}" >> "$GITHUB_OUTPUT"
+
+
+    bin_dir="yq_$(date +%s)"
+    echo "bin-dir=${bin_dir}" >> "$GITHUB_OUTPUT"
+
+    bin_path="$GITHUB_WORKSPACE/${bin_dir}"
+    echo "bin-path=${bin_path}" >> "$GITHUB_OUTPUT"
   fi
-  echo "yq-installed=${yq_installed}" >> "$GITHUB_OUTPUT"
 
   yq_temp_file_path=".data-format-converter-action-$(date +%s)"
   echo "yq-temp-file-path=${yq_temp_file_path}" >> "$GITHUB_OUTPUT"
-
-  bin_dir="yq_$(date +%s)"
-  echo "bin-dir=${bin_dir}" >> "$GITHUB_OUTPUT"
-
-  bin_path="$GITHUB_WORKSPACE/${bin_dir}"
-  echo "bin-path=${bin_path}" >> "$GITHUB_OUTPUT"
 }
 
 main "$@"
