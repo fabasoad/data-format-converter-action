cf6c12f05a2782e80687e1e7c6c7c2f8403f501d
fabasoad <fabasoad@gmail.com>
2025-01-16 02:10:05 +0900

    Fix installation 1

--- src/collect-info.sh
+++ src/collect-info.sh
@@ -10,26 +10,20 @@ main() {
   input_from="${1}"
   input_to="${2}"
 
+  if command -v yq >/dev/null 2>&1; then
+    yq_installed="true"
+    log_info "yq is found at ${yq_path}. Installation skipped."
+  else
+    yq_installed="false"
+    log_info "yq is not found. Executing installation."
+  fi
+  echo "yq-installed=${yq_installed}" >> "$GITHUB_OUTPUT"
+
   bin_dir="yq_$(date +%s)"
   echo "bin-dir=${bin_dir}" >> "$GITHUB_OUTPUT"
 
   bin_path="$GITHUB_WORKSPACE/${bin_dir}"
   echo "bin-path=${bin_path}" >> "$GITHUB_OUTPUT"
-
-  if command -v yq >/dev/null 2>&1; then
-    yq_installed="true"
-    yq_path="$(which yq)"
-    log_info "yq is found at ${yq_path}. Installation skipped."
-  else
-    yq_installed="false"
-    yq_path="${bin_path}/yq"
-    if [ "${RUNNER_OS}" = "Windows" ]; then
-      yq_path="${yq_path}.exe"
-    fi
-    log_info "yq is not found. Executing installation."
-  fi
-  echo "yq-installed=${yq_installed}" >> "$GITHUB_OUTPUT"
-  echo "yq-path=${yq_path}" >> "$GITHUB_OUTPUT"
 }
 
 main "$@"
